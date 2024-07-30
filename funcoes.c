76661afcd37eb1d15a9922370852a23f2f343225
mkpierucci <40393809+mkpierucci@users.noreply.github.com>
2024-07-30 14:15:34 -0300

    Update funcoes.c

--- funcoes.c
+++ funcoes.c
@@ -1,8 +1,3 @@
-/* PROJETO DE COMPILADORES - ETAPA 5  */
-
-/* Andrei Pochmann Koenich 	 - Matrícula 00308680 */
-/* Izaias Saturnino de Lima Neto - Matrícula 00326872 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
